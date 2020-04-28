0c83ac349bac4671072a459e3b330a1b982af930
xingyuefeng <15262870437@163.com>
2020-04-29 01:07:48 +0800

    add hook

---
+++ components/utils/utils.ts
@@ -0,0 +1,48 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+import { EventMap } from '../types/types';
+
+export const canUseDOM = typeof window !== 'undefined';
+
+export function dethunkify<T>(value: T | (() => T)): T {
+  return typeof value === 'function' ? (value as () => T)() : value;
+}
+
+export function managedEventListener<
+  T extends EventTarget,
+  K extends keyof EventMap<T> & string
+>(
+  target: T,
+  type: K,
+  callback: (event: EventMap<T>[K]) => void,
+  options?: AddEventListenerOptions,
+): () => void {
+  target.addEventListener(type, callback as EventListener, options);
+  return (): void => {
+    target.removeEventListener(type, callback as EventListener, options);
+  };
+}
+
+export function managedInterval(
+  callback: () => void,
+  delayMs: number,
+): () => void {
+  const id = setInterval(callback, delayMs);
+  return (): void => {
+    clearInterval(id);
+  };
+}
+
+export function useEventCallback<T extends Function>(
+  callback: T,
+): (...args: unknown[]) => T {
+  // Source: https://reactjs.org/docs/hooks-faq.html#how-to-read-an-often-changing-value-from-usecallback
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return useCallback((...args): T => ref.current!(...args), [ref]);
+}
--- site/pages/home/index.tsx
+++ site/pages/home/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from '@/components'
+import useEventListener from '@/components/hooks/useEventListener'
 
 export default function Home() {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  useEventListener('mousemove', (event: MouseEvent) => {
+    setCoords({x: event.pageX, y: event.pageY})
+  });
+
+
   return (
     <div className="home">
+      <h1>
+        The mouse position is ({coords.x}, {coords.y})
+    </h1>
       <div className="desc">基于TypeScript与React Hooks开发的ui组件库</div>
       <Button type="primary"><Link to="/components" className="view">查看组件</Link></Button>
     </div>
